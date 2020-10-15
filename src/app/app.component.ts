d2958b2e930449a32cf513d262ec5dfce2d4e51f
jeanmarc123456 <j.delfosse@me.com>
2020-10-15 15:44:00 +0200

    Utilisation de la changedetection strategy en onPush. Cela permet d'éviter le rechargement des composants dès qu'une de leur valeur est modifiée.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from './person';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'changedetection';
+  public person: Person = {
+    name: "John Doe",
+    age: 20
+
+  }
+
+  public reassign() {
+    this.person ={
+      name: this.person.name,
+      age: this.person.age
+    }
+  }
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Person } from '../person';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChildComponent implements OnInit {
+
+
+  @Input() person: Person;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public check() {
+    console.log("Chargement de la vue...");
+  }
+}
